85fd7c8f7132d0a9f5830c2d3f7ce8b69bce0e25
Syed Tasavour <syedtasavour86@gmail.com>
2024-08-17 22:04:57 +0530

    Day 11

---
+++ Day 11/app.js
@@ -0,0 +1,24 @@
+// let n1 = 2;
+// let n2 = 7;
+// let n3 = 9;
+
+// if(n1>n2 && n1>n3){
+//     console.log(n1);
+// }else if(n2 > n1 && n2> n3){
+//     console.log(n2);
+// }else{
+//     console.log(n3);
+// }
+
+// let x = 32;
+// let y = 47852;
+// // if(x[x.length]==y[y.length]){
+// //     console.log(same);
+// // }
+
+
+// let h = "    he  Le   ";
+// console.log(h.trim());
+let ss = "K K K K K k k k k k k higer LOWER";
+console.log(ss.toLowerCase());
+       
