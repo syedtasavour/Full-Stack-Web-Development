bb2ddbef0291a92bc115c28beb99947956cd4fc0
Syed Tasavour <syedtasavour86@gmail.com>
2024-08-18 23:22:46 +0530

    day 12

---
+++ Day 12/app.js
@@ -0,0 +1,13 @@
+// const arr = ["arr", "arr1", "arr2"]
+// console.log(arr);
+// arr.push(4);
+// arr[0] = "arr6";
+// const game = [["x", "null", "x"], ["null", "x", "null"], ["x", "null", "x"]];
+
+// const arr = [1, 2,3,4,5,6,-4,7,7,8,8,-1,-6,-7];
+// for(let i = 1;i>0;i++){
+    
+//         console.log(i);
+//         break;
+    
+// }
