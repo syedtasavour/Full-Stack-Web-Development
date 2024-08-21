2dd7f9202b6ce5dec7dc52d1be263aa926be48ea
Syed Tasavour <syedtasavour86@gmail.com>
2024-08-21 17:08:37 +0530

    Update app.js

--- Day 15/app.js
+++ Day 15/app.js
@@ -1,5 +1,67 @@
 const student = {
     name: "shradha",
-    age : 23,
-    marks : 92
+    age: 23,
+    marks: 92
 };
+
+const students = {
+    sahil: {
+        grade: "B",
+        City: "J&K",
+        Class: "5th Sem"
+    },
+    mir: {
+        grade: "D",
+        City: "J&K",
+        Class: "5th Sem"
+    },
+    bazeem: {
+        grade: "A",
+        City: "J&K",
+        Class: "5th Sem"
+    },
+    shadab: {
+        grade: "A",
+        City: "J&K",
+        Class: "5th Sem"
+    },
+    arsalan: {
+        grade: "D",
+        City: "J&K",
+        Class: "5th Sem"
+    },
+
+}
+const ArrayObject = [ 
+    {
+    Name: "Sahil",
+        grade : "B",
+            City : "J&K",
+                Class : "5th Sem"
+},
+{
+    Name: "Mir",
+        grade : "D",
+            City : "J&K",
+                Class : "5th Sem"
+},
+{
+    Name: "bazeem",
+        grade: "A",
+            City : "J&K",
+                Class : "5th Sem"
+},
+{
+    Name: "Shadab",
+        grade: "A",
+            City : "J&K",
+                Class : "5th Sem"
+},
+{
+    Name: "Arsalan",
+        grade : "D",
+            City : "J&K",
+                Class : "5th Sem"
+},
+
+];
