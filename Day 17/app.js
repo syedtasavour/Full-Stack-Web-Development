e1fce66aab99914a63239f46eea27144bbbabd74
Syed Tasavour <syedtasavour86@gmail.com>
2024-08-23 01:06:17 +0530

    Day 17

---
+++ Day 17/app.js
@@ -0,0 +1,10 @@
+function hi(a,b,c){
+    let temp = (a+b+c)/3;
+    console.log(temp);
+}
+function multi(a){
+    for(let i = 1;i<11;i++)
+        console.log(`${a} X ${i} = ${a*i}`);
+    
+}
+multi(2);
