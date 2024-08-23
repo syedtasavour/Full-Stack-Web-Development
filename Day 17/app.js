c4573194ee3f1d3c9d7f45e978023e324a8168aa
Syed Tasavour <syedtasavour86@gmail.com>
2024-08-23 12:59:11 -0700

    Day 17

--- Day 17/app.js
+++ Day 17/app.js
@@ -7,4 +7,38 @@ function multi(a){
         console.log(`${a} X ${i} = ${a*i}`);
     
 }
+function sum(a,b){
+    return a+b;
+}
 multi(2);
+
+
+function nsum (n){
+    let temp = 0;
+    for(i =1 ; i<=n;i++){
+        temp = temp+i;
+
+    }return temp;
+
+}
+ let str = ["hi","hello","bye","!"];
+ function concat(str){
+    let result;
+    for(let i = 0; i<str.length;i++){
+        result +=  " " + str[i];
+    }return result;
+ }
+
+
+console.log(concat(str));
+
+
+
+
+
+
+
+
+console.log(sum(sum(1,2),3));
+
+console.log(nsum(5));
