54e6cf95408e9dd87eb1dd898ef71133025f1233
Syed Tasavour <syedtasavour86@gmail.com>
2024-08-28 10:28:32 -0700

    Day 22

--- Day 22/app.js
+++ Day 22/app.js
@@ -14,3 +14,37 @@ const student = {
 };
 
 let {username: user, password: pass, city = "Missing"} = student;
+let n = [1,2,3,4,5,6,7,8]
+
+let d = n.map((el)=>{
+    return el *el;
+});
+
+let dd = d.reduce((sum,ew)=>{
+    return sum+ew;
+});
+
+let avg = dd/n.length;
+
+let sum = n.reduce((sum,el)=>{
+    return sum +el *el,0;
+
+});
+let newArray = n.map(element => element + 5);
+
+
+// let ddd = dd.map((el)=>{
+//     return el/n.length;
+// });
+
+function doubleAndReturnArgs(arr, ...args) {
+    // Double each of the additional arguments
+    let doubledArgs = args.map(arg => arg * 2);
+    
+    // Combine the original array with the doubled arguments
+    return [...arr, ...doubledArgs];
+}
+
+// Example usage:
+let result = doubleAndReturnArgs([1, 2, 3], 4, 5, 6);
+console.log(result); // Output will be [1, 2, 3, 8, 10, 12]
