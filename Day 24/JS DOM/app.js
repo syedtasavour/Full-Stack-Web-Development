373be0ed204d5244a62a157c91d65302275fc5f3
Syed Tasavour <syedtasavour86@gmail.com>
2024-08-30 17:06:42 -0700

    Day 24

---
+++ Day 24/JS DOM/app.js
@@ -0,0 +1,12 @@
+// let nn = document.getElementById("mainImg");
+// nn.src = "assets/creation_1.png"; 
+
+// let n = document.getElementsByClassName("oldImg");
+// for(let i =0;i<n.length;i++ ){
+//     n[i].src = "assets/spiderman_img.png";
+//     console.dir(n[i].src);
+
+// }
+// let t =document.getElementsByTagName("p");
+
+console.dir(document.querySelector("h1"));
