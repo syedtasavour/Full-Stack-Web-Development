78010379a1763209a90fada1cbebf7e3c83528ab
Syed Tasavour <syedtasavour@icloud.com>
2024-09-11 13:09:58 -0700

    Day 1 Node Js Day 36 FS

--- Day 36/ap.js
+++ Day 36/ap.js
@@ -8,3 +8,13 @@ figlet("Day 36", function (err, data) {
   }
   console.log(data);
 });
+
+
+figlet("Day ! Node Js", function (err, data) {
+  if (err) {
+    console.log("Something went wrong...");
+    console.dir(err);
+    return;
+  }
+  console.log(data);
+});
