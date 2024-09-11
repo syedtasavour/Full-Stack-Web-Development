ae906a5a8045f942c59a368915f1232ee2d87983
Syed Tasavour <syedtasavour@icloud.com>
2024-09-11 13:18:30 -0700

    Day 1 Node Js Day 36 FS

---
+++ Day 36/ap.js
@@ -0,0 +1,20 @@
+const figlet = require("figlet");
+
+figlet("Day 36", function (err, data) {
+  if (err) {
+    console.log("Something went wrong...");
+    console.dir(err);
+    return;
+  }
+  console.log(data);
+});
+
+
+figlet("Day ! Node Js", function (err, data) {
+  if (err) {
+    console.log("Something went wrong...");
+    console.dir(err);
+    return;
+  }
+  console.log(data);
+});
