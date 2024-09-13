fa9485eff37dee8d7edbcaebfc8466a923fc4f42
Syed Tasavour <syedtasavour@icloud.com>
2024-09-13 10:36:05 -0700

    day 38

---
+++ Day 38/Miscellaneous/app.js
@@ -0,0 +1,29 @@
+// let arr = [1, 2, 3, 4];
+// let arr2 = [1, 2, 3, 4];
+// arr.sayHi = () => {
+//   console.log("Hi");
+// };
+
+// const stu1 = {
+//   name: "name",
+//   age: 23,
+//   marks: 95,
+//   getMarks: function () {
+//     return this.marks;
+//   },
+// };
+// Constructors Does't return anything & Start with capital letter
+function Person(name,age){
+    this.name = name;
+    this.age = age;
+    console.log(this);
+
+
+   }
+//    let p1 = new Person("user1",1);
+//    let p2 = new Person("user2",2);
+
+
+
+// let p1 = personMaker("user1",1);
+// let p2 = personMaker("user2",2);
