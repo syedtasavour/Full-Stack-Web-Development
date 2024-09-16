2c3b70be0f599a457423de6ecc1966e22a94c17b
Syed Tasavour <syedtasavour@icloud.com>
2024-09-16 02:13:48 -0700

    day 41

--- Day 41/index.js
+++ Day 41/index.js
@@ -1,13 +1,11 @@
-const { faker } = require('@faker-js/faker');
+const { faker } = require("@faker-js/faker");
 
-let createRandomUser = () =>{
-    return {
-      userId: faker.string.uuid(),
-      username: faker.internet.userName(),
-      email: faker.internet.email(),
-      avatar: faker.image.avatar(),
-      password: faker.internet.password(),
-      birthdate: faker.date.birthdate(),
-      registeredAt: faker.date.past(),
-    };
-  }
+let getRandomUser = () => {
+  return {
+    id: faker.string.uuid(),
+    username: faker.internet.userName(),
+    email: faker.internet.email(),
+    password: faker.internet.password()
+  };
+};
+console.log(getRandomUser());
