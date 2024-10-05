4aa3ae9a4c5aaa7a8bb20080b9665394f7a99a5e
Syed Tasavour <syedtasavour@icloud.com>
2024-10-05 18:10:20 +0530

    day 56

--- Day 55/init/index.js
+++ Day 55/init/index.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const initData = require("./data.js");
 const Listing = require("../models/listing.js");
 
-const MONGO_URL = "mongodb://127.0.0.1:27017/stayfinder";
+const MONGO_URL = "mongodb+srv://stayfinder:gp1DHBj82QDio3J1qLWvv@cluster0.49ny9.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 main()
   .then(() => {
@@ -18,7 +18,7 @@ async function main() {
 
 const initDB = async () => {
   await Listing.deleteMany({});
-  initData.data = initData.data.map((obj)=>({...obj,owner:'66fe544d6bb6e44d2356df0d'}))
+  initData.data = initData.data.map((obj)=>({...obj,owner:'66ffef19105e7543c0598a37'}))
   await Listing.insertMany(initData.data);
   console.log("data was initialized");
 };
