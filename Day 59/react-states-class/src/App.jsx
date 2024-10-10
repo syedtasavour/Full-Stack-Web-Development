6369fb49d70576122b5b7e0c67ab80135754e41e
Syed Tasavoru <syedtasavour86@gmail.com>
2024-10-10 12:26:11 +0530

    day 59

---
+++ Day 59/react-states-class/src/App.jsx
@@ -0,0 +1,17 @@
+import Counter from './counter.jsx';
+import './App.css'
+
+
+function App({incCount}) {
+ 
+
+  return (
+    <>
+      <h1>States in React</h1>
+      <Counter/>
+      
+    </>
+  )
+}
+
+export default App
